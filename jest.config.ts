6c6867409ef14ee55f279bd9f3861b0e502c56f3
David <biggiegun@gmail.com>
2024-03-18 10:33:25 -0500

    Coverage validation

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageReporters: ["json", "html"]
+  coverageReporters: ["json", "html"],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
